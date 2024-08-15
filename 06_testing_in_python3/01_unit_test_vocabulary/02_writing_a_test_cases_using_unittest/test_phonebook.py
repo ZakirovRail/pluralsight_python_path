26d5cf8b56b00cbcd1344ecbc6e48e43e0aac549
Rail Zakirov <rzakirov@purestorage.com>
2024-08-15 07:43:44 +0200

    Python path course for testing using Python - finished module with Unittests

--- 06_testing_in_python3/01_unit_test_vocabulary/02_writing_a_test_cases_using_unittest/test_phonebook.py
+++ 06_testing_in_python3/01_unit_test_vocabulary/02_writing_a_test_cases_using_unittest/test_phonebook.py
@@ -5,14 +5,39 @@ from phonebook import Phonebook
 
 class PhonebookTest(unittest.TestCase):
 
+    def setUp(self) -> None:
+        self.phonebook = Phonebook()
+
+    def tearDown(self) -> None:
+        pass
+
     def test_lookup_by_name(self):
-        phonebook = Phonebook()
-        phonebook.add("Bob", "12345")
-        number = phonebook.lookup("Bob")
+        self.phonebook.add("Bob", "12345")
+        number = self.phonebook.lookup("Bob")
 
         self.assertEqual(number, "12345")
 
     def test_missing_name(self):
-        phonebook = Phonebook()
         with self.assertRaises(KeyError):
-            phonebook.lookup("missing")
+            self.phonebook.lookup("missing")
+
+    # @unittest.skip("Work In Progress")
+    def test_empty_phonebook_is_consistent(self):
+        self.phonebook = Phonebook()
+        is_consistent = self.phonebook.is_consistent()
+        self.assertTrue(is_consistent)
+
+    def test_is_consistent_with_different_entries(self):
+        self.phonebook.add("Bob", "12345")
+        self.phonebook.add("Anna", "012345")
+        self.assertTrue(self.phonebook.is_consistent())
+
+    def test_inconsistent_with_duplicate_entries(self):
+        self.phonebook.add("Bob", "12345")
+        self.phonebook.add("Sue", "12345")
+        self.assertFalse(self.phonebook.is_consistent())
+
+    def test_inconsistent_with_duplicate_prefix(self):
+        self.phonebook.add("Bob", "12345")
+        self.phonebook.add("Sue", "123")
+        self.assertFalse(self.phonebook.is_consistent())
